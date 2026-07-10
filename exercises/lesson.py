64e644c618a675ed3527c6bb95f51394d0736c6c
Harvey Medina <medinaharvey30@gmail.com>
2026-07-10 11:35:12 +0800

    Feat: add task list storage and viewing

--- exercises/lesson.py
+++ exercises/lesson.py
@@ -1,9 +1,26 @@
-while True:
-    theInput = input("Type Exit: ")
+tasks = []
 
-    if theInput == "exit":
-        print("Nice")
-    
-        break
+tasks.append("Eat")
+tasks.append("Code")
+tasks.append("Sleep")
 
-    print("Why you typed", theInput)
+print("My Tasks")
+
+for index, task in enumerate(tasks, start=1):
+    print(f"{index}. {task}")
+
+# tasks = [
+
+# ]
+
+# chosenTask = input("Task1: ")
+# tasks.append(chosenTask)
+
+# chosenTask = input("Task2: ")
+# tasks.append(chosenTask)
+
+# chosenTask = input("Task3: ")
+# tasks.append(chosenTask)
+
+# print(tasks)
+# print(len(tasks))
--- src/main.py
+++ src/main.py
@@ -14,32 +14,51 @@ print(f"Hello {user_name}")
 print()
 
 print("1. Add Task")
-print("2. Exit")
+print("2. View Tasks")
+print("3. Exit")
 
 print()
 
+tasks = []
 
 while True:
-    task_name = input("Choose an Action: ")
+    choice = input("Choose an Action: ")
 
     print()
 
-    if task_name == "2":
+    if choice == "3":
         print("Exit")
         break
 
-    elif task_name == "1":
-            chosen_task = input("What task?: ")
-            print(f"Task Added: {chosen_task}")    
+    elif choice == "1":
+        chosen_task = input("What task?: ")
+
+        print()
+
+        print(f"Task Added: {chosen_task}")
+        tasks.append(chosen_task)
+
+    elif choice == "2":
+         print("My Tasks")
+
+         if not tasks:
+            print("No Task Added Yet")
+         print()
+
+         for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
 
     else:
         print("invalid option")
 
+
     print()
 
     print("1. Add Task")
-    print("2. Exit")
-                
+    print("2. View Tasks")
+    print("3. Exit")
+
+
 
 
 
